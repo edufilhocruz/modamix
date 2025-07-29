30946a170d072723f1c73dd7731cc1e97df182bd
Eduardo Cruz <eduardocruz@fexol.com.br>
2025-07-29 12:29:27 -0300

    Fix: autilizacao administrativo

--- src/pages/admin/AdminLayout.tsx
+++ src/pages/admin/AdminLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import { AdminNavbar } from "@/components/admin/AdminNavbar";
 
 const AdminLayout = () => (
-  <div className="relative min-h-screen max-w-sm mx-auto bg-white">
+  <div className="relative min-h-screen max-w-sm mx-auto bg-white pb-24">
     <Outlet />
     <AdminNavbar />
   </div>
